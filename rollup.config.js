ec560233f1e00aede594d83331ae3d0d51440f86
Joshua Tompkins <josh@joshtompkins.com>
2017-11-26 16:02:06 -0800

    Finalize NPM and Rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,27 @@
 import resolve from 'rollup-plugin-node-resolve'
+import commonjs from 'rollup-plugin-commonjs'
 import babel from 'rollup-plugin-babel'
+import uglify from 'rollup-plugin-uglify'
+import { minify } from 'uglify-es'
 
 export default {
-  input: 'src/main.js',
-  output: {
-    file: 'dist/bundle.js',
-    format: 'cjs',
-  },
-  plugins: [resolve(), babel()],
+  input: 'src/index.js',
+  output: [
+    {
+      file: 'dist/bundle.js',
+      format: 'cjs',
+    },
+    {
+      file: 'dist/bundle.es.js',
+      format: 'es',
+    },
+  ],
+  sourcemap: true,
+  external: ['react', 'react-dom', 'prop-types'],
+  plugins: [
+    resolve({ extensions: ['.js', '.jsx'] }),
+    commonjs(),
+    babel(),
+    uglify({}, minify),
+  ],
 }
