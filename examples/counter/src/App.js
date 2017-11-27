548feb986b3379727bd9a569bb3ba92adee8a92a
Joshua Tompkins <josh@joshtompkins.com>
2017-11-26 16:03:13 -0800

    Add create-react-app example app

---
+++ examples/counter/src/App.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react'
+import { Store, JsonSerializer, Provider, connect } from 'rehash'
+
+const store = new Store({
+  count: JsonSerializer,
+})
+
+const actions = {
+  increment: state => ({ count: state.count + 1 }),
+}
+
+const mapToProps = ({ count }) => ({ count })
+
+const Counter = connect(mapToProps)(({ count, increment }) => {
+  return (
+    <div>
+      <h1>Count: {count || 0}</h1>
+      <button onClick={increment}>Increment counter</button>
+    </div>
+  )
+})
+
+class App extends Component {
+  render() {
+    return (
+      <Provider store={store}>
+        <Counter />
+      </Provider>
+    )
+  }
+}
+
+export default App
