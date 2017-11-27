575c6f49664ec1ca9134728e78a34012fbb46f48
Joshua Tompkins <josh@joshtompkins.com>
2017-11-26 16:01:03 -0800

    Rename entry point to index.js

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import Store from './store'
+import DateSerializer from './serializers/dateSerializer'
+import JsonSerializer from './serializers/jsonSerializer'
+import { Provider, Connect, connect } from './react'
+
+export { Store, DateSerializer, JsonSerializer, Provider, Connect, connect }
--- src/main.js
+++
@@ -1,5 +0,0 @@
-import Store from './store'
-import DateSerializer from './serializers/dateSerializer'
-import FakeHashRepository from './fakes/fakeHashRepository'
-
-export { Store, DateSerializer, FakeHashRepository }
